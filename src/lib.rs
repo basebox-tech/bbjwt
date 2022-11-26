d2c1988673ea49371ba7b7cf65a2d124d04c1e3b
Markus Thielen <markus.thielen@basebox.tech>
2022-11-26 09:14:46 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+//!
+//! JWT validation library for basebox (and maybe others :-) )
+//!
+//! # Synopsis
+//!
+//! This lib was created to provide a straight forward, simple and reliable way to validate
+//! JWTs against a set of public keys loaded from a URL.
+//! We use it to validate OpenID Connect ID Tokens (which are JWTs) using the set of public
+//! keys published by the OpenID server (e.g. Keycloak).
+//!
+//! It provides the following features:
+//!
+//! * Download a set of public keys from a URL (a [JSON Web Key Set](https://connect2id.com/products/server/docs/config/jwk-set))
+//! * Provide an entry point to update the keyset if necessary
+//! * Parse JWTs and validate them using the key(s) in the downloaded keyset.
+//!
+//! Basta (i.e. "that's it").
+//!
+//! **Note:** As stated above, this lib is part of an [OpenID Connect](https://openid.net/connect/)
+//! based authentication system. JWTs created and used by OpenID Connect have slightly different
+//! requirements and rules than other JWTs, and this library assumes OpenID Connect JWTs. More info
+//! about ID Tokens can be found [here](https://openid.net/specs/openid-connect-core-1_0.html#IDToken).
+//!
+//! ## Building
+//!
+//! bbjwt uses the openssl crate, so OpenSSL development libraries are required to build bbjwt. See
+//! the [openssl crate's](https://docs.rs/openssl/latest/openssl/) documentation for details.
+//!
+//! ## Why yet another Rust JWT lib?
+//!
+//! If you ever had tried to use one of the existing libraries to do JWT validation you would
+//! not ask this question.
+//!
+//! Author: Markus Thielen <markus.thielen@basebox.tech>
+//!
+//! Copyright (c) 2022 basebox GmbH, all rights reserved.
+//!
+//! License: MIT
+//!
+//! Made with ❤️ and Emacs :-)
+//!
+
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
